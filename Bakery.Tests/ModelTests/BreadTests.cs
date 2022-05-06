5964c24c72c87639ae434428df75640a4236f9c1
Sploob <crayiphone@gmail.com>
2022-05-06 09:21:51 -0700

    add teardown for testing bread list

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -1,17 +1,39 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using Bakery;
 
 namespace Bakery.Tests
 {
   [TestClass]
-  public class BreadTests
+  public class BreadTests : IDisposable
   {
+    public void Dispose()
+    {
+      Bread.ClearAll();
+    }
     [TestMethod]
     public void BreadConstructor_CreateBread_Bread()
     {
       Bread newBread = new Bread();
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+
+    [TestMethod]
+    public void GetBreadOrder_GetsBreadOrderList_ListOfBread()
+    {
+      Bread newBread = new Bread();
+
+      Assert.AreEqual(1, Bread.GetBreadOrder().Count);
+    }
+
+    // [TestMethod]
+    // public void BreadConstructor_AddsMultipleBread_ListOfBread()
+    // {
+    //   Bread newBread = new Bread(3);
+
+    //   Assert.AreEqual(3, newBread.Count());
+    // }
+
   }
 }
